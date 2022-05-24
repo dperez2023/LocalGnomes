2bf7309db5cc2982224db58325bfe0680307169d
dperezserrano <dev@dperez.eu>
2022-05-24 12:41:07 +0200

    Initial Commit

---
+++ LocalGnomes/LocalGnomesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalGnomesApp.swift
+//  LocalGnomes
+//
+//  Created by dperezserrano on 24/5/22.
+//
+
+import SwiftUI
+
+@main
+struct LocalGnomesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
