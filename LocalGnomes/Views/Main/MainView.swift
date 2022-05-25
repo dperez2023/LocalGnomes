8aaffd90403ba2b25818864ff6d5b8f43c8098b7
dperezserrano <dev@dperez.eu>
2022-05-25 14:46:54 +0200

    feat: Restructured MainView as View's modules

--- LocalGnomes/Views/Main/MainView.swift
+++ LocalGnomes/Views/Main/MainView.swift
@@ -13,36 +13,13 @@ struct MainView: View {
     var body: some View {
         NavigationView {
             ScrollView {
-                Text("Gnome's professions are the most relevant to help you on your adventure.")
-                    .font(.headline)
-                    .multilineTextAlignment(.center)
-                    .padding(.vertical)
-                Text("Use the search bar if you know their name or need to browse for an specific profession")
-                    .font(.headline)
-                    .multilineTextAlignment(.center)
-                    .padding(.vertical)
+                MainViewHeader()
                 LazyVStack {
                     ForEach(viewModel.searchResults) { gnome in
                         NavigationLink {
                             DetailsView(viewModel: DetailsView.ViewModel(gnome: gnome, gnomes: viewModel.gnomes))
                         } label: {
-                            VStack {
-                                Text(gnome.professionsList)
-                                    .font(.headline)
-                                    .foregroundColor(.white)
-                                    .padding([.horizontal])
-                                Text(gnome.longDisplayName)
-                                    .font(.subheadline)
-                                    .foregroundColor(.darkBackground)
-                            }
-                            .padding(.vertical)
-                            .frame(maxWidth: .infinity)
-                            .background(.lightBackground)
-                            .clipShape(RoundedRectangle(cornerRadius: 10))
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 10)
-                                    .stroke(.lightBackground)
-                            )
+                            MainGnomeView(gnome: gnome)
                         }
                     }
                     .searchable(text: $viewModel.searchText, prompt: "Search by name or profession")
@@ -57,6 +34,44 @@ struct MainView: View {
             .preferredColorScheme(.dark)
             .alert(viewModel.loadDataErrorMessage, isPresented: $viewModel.showLoadDataError) { }
         }
+    }
+}
+
+struct MainViewHeader: View {
+    var body: some View {
+        Text("Gnome's professions are the most relevant to help you on your adventure.")
+            .font(.headline)
+            .multilineTextAlignment(.center)
+            .padding(.horizontal)
+        Spacer()
+        Text("Use the search bar if you know their name or need to browse for an specific profession")
+            .font(.headline)
+            .multilineTextAlignment(.center)
+            .padding(.horizontal)
+    }
+}
+
+struct MainGnomeView: View {
+    var gnome: Gnome
+
+    var body: some View {
+        VStack {
+            Text(gnome.professionsList)
+                .font(.headline)
+                .foregroundColor(.white)
+                .padding([.horizontal])
+            Text(gnome.longDisplayName)
+                .font(.subheadline)
+                .foregroundColor(.darkBackground)
+        }
+        .padding(.vertical)
+        .frame(maxWidth: .infinity)
+        .background(.lightBackground)
+        .clipShape(RoundedRectangle(cornerRadius: 10))
+        .overlay(
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(.lightBackground)
+        )
     }
 }
 
